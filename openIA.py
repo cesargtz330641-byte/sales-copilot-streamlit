27bdd8ef3ae7176d4970527e46aa39f7c83ffa83
cesargtz330641-byte <cesargtz330641@gmail.com>
2026-05-29 16:31:45 -0600

    Add files via upload

--- openIA.py
+++ openIA.py
@@ -3,7 +3,7 @@ import pandas as pd
 from openai import OpenAI
 
 client = OpenAI(
-    api_key="TU_API_KEY"
+    api_key="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"
 )
 
 # =====================================
